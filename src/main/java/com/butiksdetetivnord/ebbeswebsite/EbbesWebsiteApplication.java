ea1aa926be32a92e9e6c76a2df5ffb20c8cd312b
8ruun <mbja24@student.aau.dk>
2026-01-14 17:25:28 +0100

    The skeleton is done. texts needs to be updated

---
+++ src/main/java/com/butiksdetetivnord/ebbeswebsite/EbbesWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.butiksdetetivnord.ebbeswebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbbesWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbbesWebsiteApplication.class, args);
+    }
+
+}
